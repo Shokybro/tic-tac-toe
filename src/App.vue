36f45d98769fb47ec9ceafa54118898da739fd6c
Shoktron <shokybro211@gmail.com>
2021-11-29 21:14:18 -0500

    start

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+      <div class="container">
+         <button @click="start" class="container--btn" type="button">Start game</button>
+      </div>
+      <GameSingle v-if="current === 'Game'"></GameSingle>
+  </div>
+</template>
+
+<script>
+
+import GameSingle from '@/components/GameSingle'
+export default {
+  components: {GameSingle},
+  name: 'App',
+  data () {
+    return {
+      current: "Menu"
+    }
+  },
+  methods: {
+    start () {
+      this.current = 'Game'
+      document.querySelector('.container').style.display = 'none'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: inline-block;
+    position: relative;
+    left: 50%;
+    top: 10rem;
+    transform: translate(-50%, -50%);
+
+    &--btn {
+      border: none;
+      background-color: aqua;
+      padding: 1rem 2rem;
+      font-weight: 500;
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ src/components/GameSingle.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+      <div class="cont">
+        <div class="row">
+        <div class="box top-left"><div class="cirlce"></div></div>
+        <div class="box top-middle"></div>
+        <div class="box top-right"></div>
+      </div>
+      
+      <div class="row">
+        <div class="box"></div>
+        <div class="box middle"></div>
+        <div class="box"></div>
+      </div>
+      <div class="row">
+        <div class="box bottom-left"></div>
+        <div class="box bottom-middle"></div>
+        <div class="box bottom-right"></div>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameSingle"
+}
+</script>
+
+<style lang="scss" scoped>
+.html {
+  font-size: 10px;
+}
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.row {
+  display: flex;
+}
+.box {
+  height: 10rem;
+  width: 10rem;
+  // border: 1px solid #000;
+  // background-color: red;
+  border: 1px solid transparent;
+  cursor: pointer;
+}
+.cont {
+  display: inline-block;
+  position: relative;
+  left: 50%;
+  top: 50vh;
+  transform: translate(-50%, -50%);
+}
+.top-left {
+  // border-right-color: rgb(0, 4, 255);
+  border-bottom-color: rgb(0, 4, 255);
+}
+.top-middle {
+  border-right-color: rgb(0, 4, 255);
+  border-left-color: rgb(0, 4, 255);
+  border-bottom-color: rgb(0, 4, 255);
+}
+.top-right {
+  border-bottom-color: rgb(0, 4, 255);
+}
+.middle {
+  border-bottom-color: rgb(0, 4, 255);
+  border-left-color: rgb(0, 4, 255);
+  border-right-color: rgb(0, 4, 255);
+}
+.bottom-left {
+  border-top-color: rgb(0, 4, 255);
+}
+.bottom-middle {
+  border-left-color: rgb(0, 4, 255);
+  border-right-color: rgb(0, 4, 255);
+}
+.bottom-right {
+  border-top-color: rgb(0, 4, 255);
+}
+.d-none {
+  display: none;
+}
+</style>
